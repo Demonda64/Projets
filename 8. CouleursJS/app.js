acea06215e334bc3b73f67b51ec03efa79d05056
Demonda64 <oscardemonda@outlook.fr>
2021-05-22 18:45:23 +0200

    reprise

--- 8. CouleursJS/app.js
+++ 8. CouleursJS/app.js
@@ -4,7 +4,6 @@ const btns = document.querySelectorAll('button');
 const fond = document.body;
  
 
-
 // Démarrage des familles
 let valCouleurs = ['#BA5370', '#F4E2D8'];
 let inclinaison = 45;
@@ -14,3 +13,8 @@ inputsCouleur[1].value = valCouleurs[1];
 inputsCouleur[0].style.background = valCouleurs[0];
 inputsCouleur[1].style.background = valCouleurs[1];
 fond.style.background = `linear-gradient(${inclinaison}deg, ${valCouleurs})`;
+
+
+//inclinaison 
+
+
